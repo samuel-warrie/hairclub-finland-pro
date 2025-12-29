acb5adb260517e1d460b1d1ad9a194761d35ce6e
Samuel Warrie <104434957+samuel-warrie@users.noreply.github.com>
2025-12-29 17:22:03 +0200

    Updated LanguageContext.tsx

--- src/contexts/LanguageContext.tsx
+++ src/contexts/LanguageContext.tsx
@@ -211,7 +211,7 @@ const translations = {
       welcome: 'Tervetuloa',
       fullServiceTitle: 'Täyden palvelun',
       barberShop: 'parturi-kampaamo',
-      intro: 'Hair Club on hiusten leikkauksen laatuun, tyylittelyyn, värjäyksiin, miellyttävään asiakaspalveluun ja saavutettavuuteen erikoistuva parturi-kampaamo. Olemme toimineet Oulussa vuodesta 2012 ja meille pääset aina myös ilman ajanvarausta.',
+      intro: 'Hair Club on hiusten leikkauksen laatuun,miellyttävään asiakaspalveluun ja saavutettavuuteen erikoistuva parturi-kampaamo. Olemme toimineet Oulussa vuodesta 2012 ja meille pääset aina myös ilman ajanvarausta.',
       learnStory: 'Lue Tarinaamme',
       ourExpertise: 'Asiantuntemuksemme',
       services: 'Palvelut',
