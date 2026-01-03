21891b9b8793251ff45c51ddf28903c5ab4f22ab
Samuel Warrie <104434957+samuel-warrie@users.noreply.github.com>
2026-01-03 13:33:17 +0200

    Updated Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -75,7 +75,7 @@ const Navigation = () => {
                   }`}
                   style={{
                     textShadow: location.pathname !== '/' && !isScrolled
-                      ? '0 2px 3px rgba(0, 0, 0, 0.5), 0 1px 2px rgba(0, 0, 0, 0.5)'
+                      ? '0 2px 3px rgba(0, 0, 0, 0.2), 0 1px 2px rgba(0, 0, 0, 0.2)'
                       : 'none'
                   }}
                 >
